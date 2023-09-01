21263211a921bfa7978bd41b1542523d1043dafd
Maicon Andreis <80788841+Maicon2021@users.noreply.github.com>
2023-09-01 16:29:41 -0300

    Realisando inserção do UseCase para o Produto...

--- src/infrastructure/product/repository/sequelize/product.repository.ts
+++ src/infrastructure/product/repository/sequelize/product.repository.ts
@@ -1,9 +1,13 @@
+import { Where } from "sequelize/types/utils";
 import Product from "../../../../domain/product/entity/product";
 import ProductRepositoryInterface from "../../../../domain/product/repository/product-repository.interface";
 import ProductModel from "./product.model";
+import { where } from "sequelize";
+import { string } from "yup";
+import ProductInterface from "../../../../domain/product/entity/product.interface";
 
 export default class ProductRepository implements ProductRepositoryInterface {
-  async create(entity: Product): Promise<void> {
+  async create(entity: ProductInterface): Promise<void> {
     await ProductModel.create({
       id: entity.id,
       name: entity.name,
@@ -26,8 +30,22 @@ export default class ProductRepository implements ProductRepositoryInterface {
   }
 
   async find(id: string): Promise<Product> {
-    const productModel = await ProductModel.findOne({ where: { id } });
-    return new Product(productModel.id, productModel.name, productModel.price);
+    let productModel;
+    try {
+      productModel = await ProductModel.findOne({ 
+        where: {
+          id,
+        },
+        rejectOnEmpty: true
+      });
+    } catch (error) {
+      throw new Error("Product not found");
+    }
+
+    const product = new Product(id, productModel.name, productModel.price);
+
+    return product;
+    
   }
 
   async findAll(): Promise<Product[]> {
